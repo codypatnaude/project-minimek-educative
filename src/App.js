4f0a8e0536039078e7cdb9174feadb972c3e195d
Cody Patnaude <codypatnaude@gmail.com>
2019-10-15 14:01:35 -0400

    added sample component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SampleComponent from "./components/SampleComponent";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SampleComponent></SampleComponent>
     </div>
   );
 }

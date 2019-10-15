f0fb26c87c1d71627cf70a050326aa5e7f479fd9
Cody Patnaude <codypatnaude@gmail.com>
2019-10-15 14:47:08 -0400

    added tab-bar components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
-import { Header } from "semantic-ui-react";
+import { Header, Container } from "semantic-ui-react";
+import TabBarContainer from "./features/tabs/TabBarContainer";
 
 function App() {
+  const tabs = [
+    { name: "unitInfo", label: "Unit Info" },
+    { name: "pilots", label: "Pilots" },
+    { name: "mechs", label: "Mechs" },
+    { name: "unitOrganization", label: "Unit Organization" }
+  ];
   return (
     <div className="App">
       <div className="App-header">
@@ -10,6 +17,9 @@ function App() {
           Project Mini-Mek
         </Header>
       </div>
+      <Container>
+        <TabBarContainer tabs={tabs} size="massive"></TabBarContainer>
+      </Container>
     </div>
   );
 }
